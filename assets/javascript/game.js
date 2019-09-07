09e207882196043d09842ba14f36774e33dd10fd
Lance Mok <innerlewd@gmail.com>
2019-09-07 13:30:31 -0400

    finished up html and js

---
+++ assets/javascript/game.js
@@ -0,0 +1,46 @@
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var computerGuess = computerChoices[Math.floor(Math.random()* computerChoices.length)];
+var wins = 0;
+var losses = 0;
+var guessesLeft = 10;
+var guessArray = [];
+
+
+document.onkeyup = function (event) {
+    userGuess = event.key;
+     if (guessArray.includes(userGuess)) {
+         var html = "You already guessed: [" + userGuess + "]" ;
+         return document.getElementById("game").innerHTML = html;
+     } else guessArray.push(userGuess); {}
+
+    if (computerGuess == userGuess) {
+        alert("You Win")
+        wins++;
+        guessArray = [];
+        guessesLeft = 10;
+        computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    }
+
+    if (computerGuess != userGuess) {
+        guessesLeft--;
+    }
+
+    if (guessesLeft == 0) {
+        alert("You Lose")
+        losses++;
+        guessesLeft = 10;
+        guessArray = []
+    }
+    var html = 
+        "<p>Guess what letter I'm thinking of...</p>" +
+        "<p>Wins: " + wins + "</p>" +
+        "<p>Losses: " + losses + "</p>" +
+        "<p>Guesses Left: " + guessesLeft + "</p>" +
+        "<p>Current Guesses: " + guessArray + "</p>";
+    document.getElementById("game").innerHTML = html;
+
+}
+
+
+
+
